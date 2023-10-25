527148c1231e39607a0b07bb643a6b5581ae7e0e
Steve Wilford <steve.wilford@n-able.com>
2023-10-25 16:20:37 +0100

    Target project must be built before Roslyn component execution

---
+++ Generator/DateTimeSourceGenerator.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Generator;
+
+[Generator]
+public class DateTimeSourceGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        // put a breakpoint in here
+    }
+}
